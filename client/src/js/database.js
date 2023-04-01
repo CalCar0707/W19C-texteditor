0a98e7c8e65a1e20c1eeaec238125670071e6faa
Call Carl <callienicole0419@gmail.com>
2023-04-01 14:41:43 -0500

    updated putdb and getdb methods in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,12 +14,29 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
-}
+  console.log('Post to the db');
+  const textDb = await openDB('jate', 1);
+  const tx = textDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ text: content});
+  const result = await request;
+  console.log('Data saved to the database', result);
+  //console.error('putDb not implemented');
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
-}
+  console.log('GET all from the database');
+  const textDb = await openDB('jate', 1);
+  const tx = textDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+  
+  
+  //console.error('getDb not implemented');
+};
 
 initdb();

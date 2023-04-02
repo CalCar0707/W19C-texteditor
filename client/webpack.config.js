4497c2c6c09fa09e32204d94a52b48c18e607346
Call Carl <callienicole0419@gmail.com>
2023-04-01 19:40:24 -0500

    updated webpack manifest- install working

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -45,12 +45,12 @@ module.exports = () => {
         publicPath: './',
         icons: [
           {
-            src: path.resolve('./favicon.ico'),
-            sizes: [96],
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           },
         ],
-        
+
       }),
 
     ],
